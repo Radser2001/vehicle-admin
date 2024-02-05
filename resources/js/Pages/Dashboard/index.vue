d4e9e2c919d3d426e896af31a42abb2d4906d540
Radser2001 <supuni.randeniya@gmail.com>
2024-02-05 08:43:04 +0530

    Update dashboard

--- resources/js/Pages/Dashboard/index.vue
+++ resources/js/Pages/Dashboard/index.vue
@@ -1,19 +1,50 @@
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { Head, Link } from "@inertiajs/vue3";
+import { ref, onBeforeMount } from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+library.add(faHouse);
+
+const total_vehicles = ref(Number);
+
+onBeforeMount(() => {
+    if (!window.Laravel) {
+        window.location.reload();
+    }
+});
+</script>
+
 <template>
-    <AppLayout title="Vehicle - Admin">
+    <AppLayout title="Vehicle Admin">
         <template #header>
             <div class="pb-6 header">
                 <div class="container-fluid">
                     <div class="header-body">
                         <div class="py-4 col align-items-center">
-                            <h6 class="mb-0 h2 text-dark d-inline-block">Dashboard</h6>
-                            <nav aria-label="breadcrumb" class="d-none d-md-block ">
-                                <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                            <h6 class="mb-0 h2 text-dark d-inline-block">
+                                Dashboard
+                            </h6>
+                            <nav
+                                aria-label="breadcrumb"
+                                class="d-none d-md-block"
+                            >
+                                <ol
+                                    class="breadcrumb breadcrumb-links breadcrumb-dark"
+                                >
                                     <li class="breadcrumb-item">
                                         <Link href="/">
-                                            <font-awesome-icon icon="fa-solid fa-house" color="#505050" />
+                                            <font-awesome-icon
+                                                icon="fa-solid fa-house"
+                                                color="#505050"
+                                            />
                                         </Link>
                                     </li>
-                                    <li class="breadcrumb-item active breadcrumb-text" aria-current="page">
+                                    <li
+                                        class="breadcrumb-item active breadcrumb-text"
+                                        aria-current="page"
+                                    >
                                         Dashboard
                                     </li>
                                 </ol>
@@ -33,86 +64,24 @@
                                     <div class="row">
                                         <div class="col-8">
                                             <div class="numbers">
-                                                <p class="mb-0 text-sm text-uppercase font-weight-bold">Total Materials
+                                                <p
+                                                    class="mb-0 text-sm text-uppercase font-weight-bold"
+                                                >
+                                                    Total Vehicles
                                                 </p>
                                                 <h5 class="font-weight-bolder">
-                                                    {{ this.total_materials }}
+                                                    {{ total_vehicles }}
                                                 </h5>
                                             </div>
                                         </div>
                                         <div class="col-4 text-end">
                                             <div
-                                                class="text-center icon icon-shape bg-gradient-danger shadow-danger rounded-circle">
-                                                <i class="text-lg ni ni-world opacity-10" aria-hidden="true"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-12">
-                            <div class="mb-4 card">
-                                <div class="p-3 card-body">
-                                    <div class="row">
-                                        <div class="col-8">
-                                            <div class="numbers">
-                                                <p class="mb-0 text-sm text-uppercase font-weight-bold">Total BOM Sheets
-                                                </p>
-                                                <h5 class="font-weight-bolder">
-                                                    {{ this.total_boms }}
-                                                </h5>
-                                            </div>
-                                        </div>
-                                        <div class="col-4 text-end">
-                                            <div
-                                                class="text-center icon icon-shape bg-gradient-primary shadow-primary rounded-circle">
-                                                <i class="text-lg ni ni-money-coins opacity-10" aria-hidden="true"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-12">
-                            <div class="mb-4 card">
-                                <div class="p-3 card-body">
-                                    <div class="row">
-                                        <div class="col-8">
-                                            <div class="numbers">
-                                                <p class="mb-0 text-sm text-uppercase font-weight-bold">Total Vendors
-                                                </p>
-                                                <h5 class="font-weight-bolder">
-                                                    {{ this.total_vendors }}
-                                                </h5>
-                                            </div>
-                                        </div>
-                                        <div class="col-4 text-end">
-                                            <div
-                                                class="text-center icon icon-shape bg-gradient-success shadow-success rounded-circle">
-                                                <i class="text-lg ni ni-paper-diploma opacity-10"
-                                                    aria-hidden="true"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-12">
-                            <div class="mb-4 card">
-                                <div class="p-3 card-body">
-                                    <div class="row">
-                                        <div class="col-8">
-                                            <div class="numbers">
-                                                <p class="mb-0 text-sm text-uppercase font-weight-bold">Total PO</p>
-                                                <h5 class="font-weight-bolder">
-                                                    {{ this.total_pos }}
-                                                </h5>
-                                            </div>
-                                        </div>
-                                        <div class="col-4 text-end">
-                                            <div
-                                                class="text-center icon icon-shape bg-gradient-warning shadow-warning rounded-circle">
-                                                <i class="text-lg ni ni-cart opacity-10" aria-hidden="true"></i>
+                                                class="text-center icon icon-shape bg-gradient-danger shadow-danger rounded-circle"
+                                            >
+                                                <i
+                                                    class="text-lg ni ni-world opacity-10"
+                                                    aria-hidden="true"
+                                                ></i>
                                             </div>
                                         </div>
                                     </div>
@@ -126,37 +95,8 @@
     </AppLayout>
 </template>
 
-<script>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import { Head, Link } from '@inertiajs/vue3'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
-
-export default {
-    components: {
-        AppLayout,
-        Link,
-        library,
-    },
-    props: {
-        total_materials: Number,
-        total_boms: Number,
-        total_vendors: Number,
-        total_pos: Number,
-    },
-    beforeMount() {
-        library.add(faHouse);
-
-        if(!window.Laravel){
-            window.location.reload();
-        }
-    },
-}
-</script>
-
 <style lang="scss" scoped>
-.breadcrumb-text{
+.breadcrumb-text {
     color: #6343e9 !important;
 }
 </style>
